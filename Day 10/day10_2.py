884d4a491c579ed51cd972d9f75e3532e9713ebf
Ture Hassler <ture.hassler@gmail.com>
2022-12-10 16:51:45 +0100

    cleanup

--- Day 10/day10_2.py
+++ Day 10/day10_2.py
@@ -1,33 +1,16 @@
-
-
-
 with open('Day 10/day10.txt') as f:
 
     data = f.readlines()
 
     image = []
-
-    t_0 = 0
-    t_1 = 1
-    t_2 = 0
     x = 1
-    sum = 0
     clock = 0
 
     def check(i, x, list):
         if (i % 40) == x-1 or (i % 40) == x or (i % 40) == x+1:
             list.append('#')
-            if i < 20:
-                print(list)
-                print('x, i: ', x, i)
-                print('------------------')
-            
         else:
             list.append(' ')
-            if i < 20:
-                print(list)
-                print('x, i: ', x, i)
-                print('------------------')
         return list
 
     for d in data:
@@ -35,21 +18,15 @@ with open('Day 10/day10.txt') as f:
         command = line[0]
 
         if command == "addx":
-            num = int(line[1])
-            
+            num = int(line[1])     
             image = check(clock, x, image)
-            clock += 1
-            
-            
+            clock += 1 
             image = check(clock, x, image)
             x += num
             clock += 1
-
-        else:
-            
+        else: 
             image = check(clock, x, image)
             clock += 1
-
 
 print(len(image))
 print(''.join(image[:40]))
